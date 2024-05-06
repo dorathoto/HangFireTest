ce526f5ed1d7f5c24473984a50f789a7293559ac
Leonardo Dorathoto <leonardo@wmb.com.br>
2024-05-06 17:37:19 -0300

    versão básica do hangfire

---
+++ HangFireTest/Models/TaskSample.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire.Server;
+
+namespace HangFireTest.Models
+{
+    public class TaskSample
+    {
+        public void TaskMethod()
+        {
+            Console.WriteLine("Testando qualquer coisa!!!");
+        }
+
+        public void TaskMethod2(PerformContext? pfContext)
+        {
+            //pfContext.LogInformation("Testing Web Sample!!!");
+            Console.WriteLine("Testandoooo!!!");
+        }
+    }
+}
--- HangFireTest/Program.cs
+++ HangFireTest/Program.cs
@@ -1,34 +1,67 @@
 
+using Hangfire;
+using Hangfire.Server;
+using Hangfire.Storage.SQLite;
+using HangFireTest.Models;
+
 namespace HangFireTest
 {
     public class Program
     {
+        [Obsolete]
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+
+            //ativando hangfire
+            builder.Services.AddHangfire(configuration => configuration
+                .UseRecommendedSerializerSettings()
+                .UseSQLiteStorage("Hangfire.db")); //poderia ser o MSSQL do projeto
+
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseHangfireDashboard(); //dashboad do hangfire
 
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
+         
+
+            var jobId = BackgroundJob.Enqueue(() => Console.WriteLine("Executando agora"));
+
+            //agendamentos
+            BackgroundJob.Schedule(
+                        () => Console.WriteLine("Job Agendado, 2  minutos após o início da aplicação"),
+                        TimeSpan.FromMinutes(2));
+
+
+            var rnd = new Random().Next(1, 200);
+            RecurringJob.AddOrUpdate(
+                        "Meu job recorrente",
+                        () => Console.WriteLine((rnd % 2 == 0)
+                            ? "Job recorrente gerou um número par"
+                            : "Job recorrente gerou um número ímpar"),
+                        Cron.Minutely,
+                        TimeZoneInfo.Local);
+
+
+            RecurringJob.AddOrUpdate("TaskMethod()", (TaskSample t) => t.TaskMethod(), Cron.Minutely);
+            RecurringJob.AddOrUpdate("TaskMethod2()", (TaskSample t) => t.TaskMethod2(null), Cron.Minutely);
+
+            var t = app.Services.GetService<IBackgroundJobClient>();
+            t.Enqueue(queue: "test_queue_1", methodCall: () => Console.WriteLine("Testing......"));
+            t.Enqueue(queue: "test_queue_1", methodCall: () => Console.WriteLine("Testing......"));
+            t.Enqueue(queue: "test_queue_1", methodCall: () => Console.WriteLine("Testing......"));
+            t.Enqueue(queue: "test_queue_1", methodCall: () => Console.WriteLine("Testing......"));
+            t.Enqueue(queue: "test_queue_1", methodCall: () => Console.WriteLine("Testing......"));
+            t.Enqueue(queue: "test_queue_1", methodCall: () => Console.WriteLine("Testing......"));
 
             app.Run();
         }
